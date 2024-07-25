e6c0260983699eecb8e3385280a37d2d416dfe1a
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-07-25 14:29:30 +0200

    replacing items require on line 3 from json to js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const itemsTab = require("./items");
+const itemsTab = require("./items.js");
 
 const app = express();
 
@@ -10,21 +10,21 @@ app.use(cors("*")); //middleware
 app.use("/static", express.static(__dirname + "/assets"));
 
 app.get("/", (req, res) => {
-    res.send("Hello la Dream Team");
+  res.send("Hello la Dream Team");
 });
 
-app.get("/items", (req,res) => {
-    res.json(itemsTab);
+app.get("/items", (req, res) => {
+  res.json(itemsTab);
 });
 
 app.get("/items/:id", (req, res) => {
-    const itemsId = parseInt(req.params.id)
-    console.info(itemsId);
-    const items = itemsTab.find((e) => e.id === itemsId);
-    console.info(items);
-    res.json(items);
+  const itemsId = parseInt(req.params.id);
+  console.info(itemsId);
+  const items = itemsTab.find((e) => e.id === itemsId);
+  console.info(items);
+  res.json(items);
 });
 
 app.listen(serverPort, () => {
-    console.info(`http://localhost:${serverPort}`);
+  console.info(`http://localhost:${serverPort}`);
 });
