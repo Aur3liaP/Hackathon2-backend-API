78e1ffe1a6a049129106aaa6092189acf9ed9f4d
Pic Aurélia <pic.aurelia@gmail.com>
2024-07-25 13:10:40 +0200

    add pictures

--- index.js
+++ index.js
@@ -24,37 +24,6 @@ app.get("/items/:id", (req, res) => {
     res.json(items);
 });
 
-// app.get("/characters/:id", (req, res) => {
-//     const characterId = parseInt(req.params.id);
-//     let character;
-//     filmsTab.some(film => {
-//         character = film.characters.find((char) => char.id === characterId);
-//         return character;
-//     });
-//     res.json(character);
-// });
-
-// app.get("/films/:filmId/characters/:characterId?", (req, res) => {
-//     const filmId = parseInt(req.params.filmId);
-//     const characterId = req.params.characterId ? parseInt(req.params.characterId) : null;
-
-//     const film = filmsTab.find((e) => e.id === filmId);
-//     if (!film) {
-//         return res.status(404).json({ error: "Film not found" });
-//     }
-
-//     else if (characterId) {
-//         const character = film.characters.find((char) => char.id === characterId);
-//         if (!character) {
-//             return res.status(404).json({ error: "Character not found" });
-//         }
-//         res.json(character);
-//     } else {
-//         // Si characterId n'est pas défini, retourner le film entier
-//         res.json(film);
-//     }
-// });
-
 app.listen(serverPort, () => {
     console.info(`http://localhost:${serverPort}`);
 });
