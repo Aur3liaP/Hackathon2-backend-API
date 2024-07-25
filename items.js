907d0d267c9f717d67209fb9f0652b49ba520c08
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-07-25 19:02:45 +0200

    english names

--- items.js
+++ items.js
@@ -1,14 +1,14 @@
 const items = [
   {
     id: 1,
-    nom: "Furby",
     description:
       "Ce petit animal de compagnie électronique est super interactif ! Il parle, bouge ses oreilles et peut même chanter !",
-    argument_de_vente:
-      "Un compagnon interactif qui réagit à ta voix et te divertit avec des expressions et des sons uniques.",
     image: "http://localhost:3310/static/furby(API).jpg",
-    prix: 399,
-    commentaires_client: [
+    name: "Furby",
+    sales_pitch:
+      "Un compagnon interactif qui réagit à ta voix et te divertit avec des expressions et des sons uniques.",
+    price: 399,
+    client_commentary: [
       {
         nom_client: "Sophie",
         commentaire:
@@ -23,14 +23,14 @@ const items = [
   },
   {
     id: 2,
-    nom: "Game Boy",
     description:
       "La Game Boy est une console de jeu portable qui te permet de jouer à tes jeux préférés partout où tu vas, comme *Tetris* et *Pokémon* !",
-    argument_de_vente:
-      "La première console de jeu portable qui t'offre des heures de divertissement avec des titres emblématiques, tout en étant facile à transporter.",
     image: "http://localhost:3310/static/Game-boy(API).jpg",
-    prix: 699,
-    commentaires_client: [
+    name: "Game Boy",
+    sales_pitch:
+      "La première console de jeu portable qui t'offre des heures de divertissement avec des titres emblématiques, tout en étant facile à transporter.",
+    price: 699,
+    client_commentary: [
       {
         nom_client: "Claire",
         commentaire:
@@ -45,14 +45,14 @@ const items = [
   },
   {
     id: 3,
-    nom: "Pog",
     description:
       "Les pogs sont des petits disques amusants que tu peux collectionner et utiliser pour jouer à des jeux passionnants avec tes amis !",
-    argument_de_vente:
-      "Un jeu de collection populaire qui encourage les échanges et les défis entre amis, tout en offrant une multitude de designs amusants.",
     image: "http://localhost:3310/static/POG(API).jpg",
-    prix: 50,
-    commentaires_client: [
+    name: "Pog",
+    sales_pitch:
+      "Un jeu de collection populaire qui encourage les échanges et les défis entre amis, tout en offrant une multitude de designs amusants.",
+    price: 50,
+    client_commentary: [
       {
         nom_client: "Elodie",
         commentaire:
@@ -67,14 +67,14 @@ const items = [
   },
   {
     id: 4,
-    nom: "Pez",
     description:
       "Les distributeurs PEZ sont des jouets amusants en forme de personnages qui distribuent des bonbons délicieux en forme de petites pastilles !",
-    argument_de_vente:
-      "Des distributeurs ludiques en forme de personnages célèbres qui rendent les bonbons encore plus amusants à manger.",
     image: "http://localhost:3310/static/pez-pikachu-orange(API).png",
-    prix: 30,
-    commentaires_client: [
+    name: "Pez",
+    sales_pitch:
+      "Des distributeurs ludiques en forme de personnages célèbres qui rendent les bonbons encore plus amusants à manger.",
+    price: 30,
+    client_commentary: [
       {
         nom_client: "Amélie",
         commentaire:
@@ -89,14 +89,14 @@ const items = [
   },
   {
     id: 5,
-    nom: "Tamagotchi",
     description:
       "Le Tamagotchi est un animal de compagnie virtuel que tu peux nourrir, jouer avec et soigner. C'est comme avoir un vrai animal de compagnie !",
-    argument_de_vente:
-      "Un animal de compagnie virtuel qui te permet de vivre l'expérience de l'entretien d'un animal tout en étant compact et portable.",
     image: "http://localhost:3310/static/Tamagochi.png",
-    prix: 250,
-    commentaires_client: [
+    name: "Tamagotchi",
+    sales_pitch:
+      "Un animal de compagnie virtuel qui te permet de vivre l'expérience de l'entretien d'un animal tout en étant compact et portable.",
+    price: 250,
+    client_commentary: [
       {
         nom_client: "Isabelle",
         commentaire:
@@ -111,14 +111,14 @@ const items = [
   },
   {
     id: 6,
-    nom: "Bipper",
     description:
       "Le bipper est un petit appareil qui t'envoie des alertes et des messages. C'est super pratique avant l'arrivée des téléphones portables !",
-    argument_de_vente:
-      "Un outil pratique pour recevoir des alertes et des messages instantanément, avant l'ère des téléphones mobiles.",
     image: "http://localhost:3310/static/Bipper3(API).jpg",
-    prix: 150,
-    commentaires_client: [
+    name: "Bipper",
+    sales_pitch:
+      "Un outil pratique pour recevoir des alertes et des messages instantanément, avant l'ère des téléphones mobiles.",
+    price: 150,
+    client_commentary: [
       {
         nom_client: "Nadine",
         commentaire:
@@ -133,14 +133,14 @@ const items = [
   },
   {
     id: 7,
-    nom: "Minitel",
     description:
       "Le Minitel est un terminal qui te permet d'accéder à des services en ligne comme les annuaires téléphoniques et même de faire des réservations !",
-    argument_de_vente:
-      "Un précurseur d'Internet qui te permet d'accéder à divers services en ligne depuis le confort de chez toi.",
     image: "http://localhost:3310/static/minitel2(API).png",
-    prix: 2000,
-    commentaires_client: [
+    name: "Minitel",
+    sales_pitch:
+      "Un précurseur d'Internet qui te permet d'accéder à divers services en ligne depuis le confort de chez toi.",
+    price: 2000,
+    client_commentary: [
       {
         nom_client: "Monique",
         commentaire:
@@ -155,14 +155,14 @@ const items = [
   },
   {
     id: 8,
-    nom: "Magnétoscope / VHS",
     description:
       "Le magnétoscope est parfait pour regarder et enregistrer des films et des émissions sur des cassettes VHS. C'est super pratique pour le divertissement à la maison !",
-    argument_de_vente:
-      "Un dispositif essentiel pour enregistrer et visionner des émissions et des films à la demande, tout en offrant une flexibilité de visionnage.",
     image: "http://localhost:3310/static/magnetoscope(API).jpg",
-    prix: 1500,
-    commentaires_client: [
+    name: "Magnétoscope / VHS",
+    sales_pitch:
+      "Un dispositif essentiel pour enregistrer et visionner des émissions et des films à la demande, tout en offrant une flexibilité de visionnage.",
+    price: 1500,
+    client_commentary: [
       {
         nom_client: "Elena",
         commentaire:
@@ -177,14 +177,14 @@ const items = [
   },
   {
     id: 9,
-    nom: "Walkman",
     description:
       "Le Walkman est un lecteur de cassettes portable qui te permet d'écouter de la musique où que tu sois. Parfait pour les déplacements !",
-    argument_de_vente:
-      "Le premier lecteur de musique portable qui te permet de profiter de ta musique préférée en déplacement, avec une qualité sonore appréciable.",
     image: "http://localhost:3310/static/lecteurcd(API).png",
-    prix: 400,
-    commentaires_client: [
+    name: "Walkman",
+    sales_pitch:
+      "Le premier lecteur de musique portable qui te permet de profiter de ta musique préférée en déplacement, avec une qualité sonore appréciable.",
+    price: 400,
+    client_commentary: [
       {
         nom_client: "Julie",
         commentaire:
@@ -199,14 +199,14 @@ const items = [
   },
   {
     id: 10,
-    nom: "Nokia 3310",
     description:
       "Le Nokia 3310 est un téléphone mobile ultra-résistant avec une batterie qui dure des semaines. Idéal pour rester en contact sans souci !",
-    argument_de_vente:
-      "Un téléphone mobile robuste et fiable avec une batterie de longue durée, parfait pour une utilisation quotidienne sans tracas.",
     image: "http://localhost:3310/static/nokia-3310(API).png",
-    prix: 1200,
-    commentaires_client: [
+    name: "Nokia 3310",
+    sales_pitch:
+      "Un téléphone mobile robuste et fiable avec une batterie de longue durée, parfait pour une utilisation quotidienne sans tracas.",
+    price: 1200,
+    client_commentary: [
       {
         nom_client: "Catherine",
         commentaire:
@@ -221,14 +221,14 @@ const items = [
   },
   {
     id: 11,
-    nom: "PlayStation",
     description:
       "La PlayStation est la première console de jeu de Sony avec des graphismes en 3D impressionnants et des jeux incroyables comme *Final Fantasy VII*. C'est le futur du jeu vidéo !",
-    argument_de_vente:
-      "Une console révolutionnaire qui apporte des graphismes 3D époustouflants et une bibliothèque de jeux impressionnante, redéfinissant le divertissement à domicile.",
     image: "http://localhost:3310/static/ps1(API).png",
-    prix: 1999,
-    commentaires_client: [
+    name: "PlayStation",
+    sales_pitch:
+      "Une console révolutionnaire qui apporte des graphismes 3D époustouflants et une bibliothèque de jeux impressionnante, redéfinissant le divertissement à domicile.",
+    price: 1999,
+    client_commentary: [
       {
         nom_client: "Lucie",
         commentaire:
@@ -243,14 +243,14 @@ const items = [
   },
   {
     id: 12,
-    nom: "TV tube cathodique",
     description:
       "La télévision à tube cathodique est le standard pour les émissions et les films avant l'arrivée des écrans plats. Elle offre une bonne qualité d'image pour son époque !",
-    argument_de_vente:
-      "Un téléviseur classique qui fournit une bonne qualité d'image et un divertissement visuel fiable avec les technologies de l'époque.",
     image: "http://localhost:3310/static/tv(API).png",
-    prix: 3500,
-    commentaires_client: [
+    name: "TV tube cathodique",
+    sales_pitch:
+      "Un téléviseur classique qui fournit une bonne qualité d'image et un divertissement visuel fiable avec les technologies de l'époque.",
+    price: 3500,
+    client_commentary: [
       {
         nom_client: "Monique",
         commentaire:
@@ -265,14 +265,14 @@ const items = [
   },
   {
     id: 13,
-    nom: "Pollypocket",
     description:
       "Les Polypocket sont des petites maisons ou véhicules miniatures avec des personnages et des accessoires que tu peux emporter partout !",
-    argument_de_vente:
-      "Des jouets miniatures qui combinent jeu de rôle et transportabilité, permettant aux enfants d'emporter leurs mondes imaginaires partout.",
     image: "http://localhost:3310/static/pollypocket(API).png",
-    prix: 150,
-    commentaires_client: [
+    name: "Pollypocket",
+    sales_pitch:
+      "Des jouets miniatures qui combinent jeu de rôle et transportabilité, permettant aux enfants d'emporter leurs mondes imaginaires partout.",
+    price: 150,
+    client_commentary: [
       {
         nom_client: "Amandine",
         commentaire:
@@ -287,14 +287,14 @@ const items = [
   },
   {
     id: 14,
-    nom: "Light Gun PS1",
     description:
       "Le Light Gun pour PlayStation 1 est un pistolet électronique utilisé pour les jeux de tir sur ton téléviseur. C'est une expérience de jeu unique !",
-    argument_de_vente:
-      "Un accessoire de jeu innovant qui offre une immersion accrue dans les jeux de tir, avec une interaction physique directe avec le téléviseur.",
     image: "http://localhost:3310/static/lightgun_1(API).png",
-    prix: 250,
-    commentaires_client: [
+    name: "Light Gun PS1",
+    sales_pitch:
+      "Un accessoire de jeu innovant qui offre une immersion accrue dans les jeux de tir, avec une interaction physique directe avec le téléviseur.",
+    price: 250,
+    client_commentary: [
       {
         nom_client: "Chloé",
         commentaire:
@@ -308,5 +308,3 @@ const items = [
     ],
   },
 ];
-
-module.exports = items;
