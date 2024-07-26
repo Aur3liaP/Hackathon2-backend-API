e7e7a5f5f095b0097b0de6d80ff54698fee62486
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-07-26 14:16:30 +0200

    re re re ok

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,11 @@
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
 
-const app = require("../app");
+// const app = require("../app");
+
+// module.exports.handler = serverless(app);
+
+
+const serverless = require("serverless-http");
+const app = require("../app");  // Adjust the path according to your app's structure
 
 module.exports.handler = serverless(app);
